48fcf8cc46ad936cea633fce5be4857bb95b7cf3
urkekg <urkekg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-01 15:59:21 +0000

    * Fix: fatal error - broken execution for embedded playlist with enhanced privacy * Add: button to discard warning notice for Redux Framework
    git-svn-id: https://plugins.svn.wordpress.org/youtube-channel/tags/2.4.0.1@999909 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/settings_usage.php
@@ -0,0 +1,5 @@
+<p>You can insert YouTube Channel in couple different ways.</p>
+<ol>
+<li>Insert <a href="widget.php">YouTube Channel Widget</a> in preferred Widget Area, and configure it there.</li>
+<li>Insert shortcode <code>[youtube_channel]</code> to your page or text widget, and even modify default settings by shortcode parameters listed in section below.</li>
+</ol>
